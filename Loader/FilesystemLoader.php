81eb9c2e42b60e46d37bd8770b794ecaf30bda91
Paul Redmond <paulrredmond@gmail.com>
2012-02-04 02:13:45 -0700

    Adding in a couple core Classes, and introducing namespace into this plugin. Setting required PHP version to 5.3.0 for now

--- Loader/FilesystemLoader.php
+++ Loader/FilesystemLoader.php
@@ -1,9 +1,11 @@
 <?php
 
+namespace TwigPlugin\Loader;
+
 /**
  * Extends the default Twig filesystem loader
  * to work with CakePHP paths.
  */
-class FilesystemLoader extends Twig_Loader_Filesystem
+class FilesystemLoader extends \Twig_Loader_Filesystem
 {	
 }
